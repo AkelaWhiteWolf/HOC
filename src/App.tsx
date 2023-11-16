3a2896fea17c7d42d7026fc8a9c45de64497b610
Alexey Shvets <alexey199822la@gmail.com>
2023-11-16 15:10:45 +0200

    main/all works

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import './App.css';
+const withTruncateParagraph = (
+  Component: React.FC<{ children: string }>,
+  limit = 10,
+) => {
+  const wrapper = ({ children, ...rest }) => {
+    const substring: string = children.substring(0, limit);
+
+    console.log({ children, substring });
+    return <Component {...rest}>{substring}</Component>;
+  };
+
+  wrapper.displayName = `WithTruncateParagraph(${
+    Component.displayName || Component.name || 'Component'
+  })`;
+  return wrapper;
+};
+
+const Paragraph = (props) => <p>{props.children}</p>;
+
+const ShortParagraph = withTruncateParagraph(Paragraph);
 
 function App() {
-  return <div>Hello, world</div>;
+  return (
+    <ShortParagraph>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, adipisci.
+    </ShortParagraph>
+  );
 }
 
 export default App;
